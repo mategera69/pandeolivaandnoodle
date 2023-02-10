b645bfb8950e44ab7add4b1cea0a500591f0f102
mategera69 <120091537+mategera69@users.noreply.github.com>
2023-02-10 09:58:44 +0100

    Add files via upload

--- jscript.js
+++ jscript.js
@@ -2,3 +2,13 @@ function DarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+} 
+function Order() {
+  window.open('mailto:geramate1@gmail.com');
+}
